be221fe943bcc6ee2dcd73b6998da50747b1c1ae
didiid <69471585+didiid@users.noreply.github.com>
2024-09-15 00:15:19 +0200

    module

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,4 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { HeaderComponent } from './app/header/header.component';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
-
-bootstrapApplication(HeaderComponent);
+platformBrowserDynamic().bootstrapModule(AppModule);
