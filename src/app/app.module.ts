a6664431f9b7e5b38a9906d5b23c762a7cff8e5a
didiid <69471585+didiid@users.noreply.github.com>
2024-09-24 14:24:38 +0200

    Module all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { SharedModule } from './shared/card/shared.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { CardComponent } from './shared/card/card.component';
-import { TaskComponent } from './tasks/task/task.component';
-import { NewTaskkComponent } from './tasks/new-taskk/new-taskk.component';
+import { TasksModule } from './tasks/tasks.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    UserComponent,
-    CardComponent,
-    TaskComponent,
-    TasksComponent,
-    NewTaskkComponent,
-  ],
+  declarations: [AppComponent, HeaderComponent, UserComponent],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, SharedModule, TasksModule],
 })
 export class AppModule {}
